f8c00d369f3c385b505df586565d3eff5a992c7b
Abhishek dubey <abhishekd905@gmail.com>
2021-12-29 23:11:45 +0530

    Add project files.

---
+++ TaskPaymentProcesses/PaymentProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPaymentProcesses
+{
+    public class PaymentProcessor
+    {
+        public void CreditCard()
+        {
+
+        }
+
+        public void DebitCard()
+        {
+
+        }
+
+        public void Netbanking()
+        {
+
+        }
+
+        public void UPIPayments()
+        {
+
+        }
+    }
+}
